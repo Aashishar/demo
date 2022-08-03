a03ca9395c10835251424d8ffb328f8e46380ae3
aashish <itsmeaashisharyal@gmail.com>
2022-08-03 13:21:36 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/example/application_name/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.application_name
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myapp());
+}
+
+class myapp extends StatelessWidget {
+  const myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: myhome(),
+    );
+  }
+}
+
+class myhome extends StatefulWidget {
+  const myhome({Key? key}) : super(key: key);
+
+  @override
+  State<myhome> createState() => _myhomeState();
+}
+
+class _myhomeState extends State<myhome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [],
+      ),
+    );
+  }
+}
